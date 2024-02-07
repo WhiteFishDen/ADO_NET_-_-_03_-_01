1607581758b3326ce6b665793b484f50162fe403
WhiteFishDen <muslinazavtrak@gmail.com>
2024-02-07 23:57:57 +0300

    Добавьте файлы проекта.

---
+++ ADO_NET_ДЗ_Модуль_03_часть_01/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace ADO_NET_ДЗ_Модуль_03_часть_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_insert = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btn_show = new System.Windows.Forms.Button();
+            this.textBox_provider = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(23, 195);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(750, 250);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btn_insert
+            // 
+            this.btn_insert.Location = new System.Drawing.Point(43, 37);
+            this.btn_insert.Name = "btn_insert";
+            this.btn_insert.Size = new System.Drawing.Size(75, 23);
+            this.btn_insert.TabIndex = 1;
+            this.btn_insert.Text = "INSERT";
+            this.btn_insert.UseVisualStyleBackColor = true;
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Location = new System.Drawing.Point(124, 36);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(75, 23);
+            this.btn_delete.TabIndex = 1;
+            this.btn_delete.Text = "DELETE";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            // 
+            // btn_update
+            // 
+            this.btn_update.Location = new System.Drawing.Point(205, 36);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(75, 23);
+            this.btn_update.TabIndex = 1;
+            this.btn_update.Text = "UPDATE";
+            this.btn_update.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(450, 38);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(284, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btn_show
+            // 
+            this.btn_show.Location = new System.Drawing.Point(286, 36);
+            this.btn_show.Name = "btn_show";
+            this.btn_show.Size = new System.Drawing.Size(75, 23);
+            this.btn_show.TabIndex = 3;
+            this.btn_show.Text = "SHOW";
+            this.btn_show.UseVisualStyleBackColor = true;
+            this.btn_show.Click += new System.EventHandler(this.btn_show_Click);
+            // 
+            // textBox_provider
+            // 
+            this.textBox_provider.Location = new System.Drawing.Point(113, 101);
+            this.textBox_provider.Name = "textBox_provider";
+            this.textBox_provider.Size = new System.Drawing.Size(660, 20);
+            this.textBox_provider.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Connection String";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_provider);
+            this.Controls.Add(this.btn_show);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btn_update);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_insert);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "DbProviderFactory";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_insert;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.Button btn_update;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btn_show;
+        private System.Windows.Forms.TextBox textBox_provider;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ADO_NET_ДЗ_Модуль_03_часть_01/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Common;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace ADO_NET_ДЗ_Модуль_03_часть_01
+{
+    public partial class Form1 : Form
+    {
+        DbConnection conn = null;
+        DbProviderFactory factory = null;
+        string providerName = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataTable tableFactory = DbProviderFactories.GetFactoryClasses();
+            dataGridView1.DataSource = tableFactory;
+            comboBox1.Items.Clear();
+            foreach (DataRow dr in tableFactory.Rows)
+            { comboBox1.Items.Add(dr["InvariantName"]); }
+        }
+        
+        private void btn_show_Click(object sender, EventArgs e)
+        {
+            conn.ConnectionString = textBox_provider.Text;
+;
+            DbDataAdapter da = factory.CreateDataAdapter();
+            da.SelectCommand = conn.CreateCommand();
+            da.SelectCommand.CommandText = "SELECT * FROM vegetables_and_fruits";
+            DataTable dt = new DataTable(); 
+            da.Fill(dt);
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = dt;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            factory = DbProviderFactories.GetFactory(comboBox1.SelectedItem.ToString());
+            conn = factory.CreateConnection();
+            providerName = GetConnectionStringByProvider(comboBox1.SelectedItem.ToString());
+            textBox_provider.Text = providerName;//typeof(Npgsql.NpgsqlFactory).AssemblyQualifiedName
+
+        }
+
+        static string GetConnectionStringByProvider(string InvariantNameProvider)
+        {
+            string returnValue = null;
+            var settings = ConfigurationManager.ConnectionStrings;
+            if(settings != null)
+            {
+                foreach (ConnectionStringSettings item in settings)
+                {
+                    if(item.ProviderName == InvariantNameProvider)
+                    {
+                        returnValue = item.ConnectionString;
+                        break;
+                    }
+                }
+            }
+            return returnValue;
+        }
+
+    }
+}
